a13936ee79e9e872c37c0561e89e113596434d83
1211102975 <123615485+1211102975@users.noreply.github.com>
2023-02-01 19:38:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 // Year: Trimester 1, 2022/23 (T2215) 
 // Lab: TT4L 
 // Names: Loi Xinyi | Lam Yuet Xin |  Mischelle Thanusha Julius 
-// IDs: 1211102975 | MEMBER_ID_2  | 1211101153
-// Emails: 1211102975@student.mmu.edu.my | MEMBER_EMAIL_2 |
+// IDs: 1211102975 | 1211101775  | 1211101153
+// Emails: 1211102975@student.mmu.edu.my |1211101775@student.mmu.edu.my |1211101153@student.mmu.edu.my
 // Phones: 0174886183 | 0176308285 | 01135632032
 // ********************************************************* 
 
@@ -18,24 +18,17 @@
 #include "AVZ.hpp"
 using namespace std;
 
-int getColumnCount();
-
-int getRowCount();
 
 int main()
 {
 	cout << "Assignment (Part 1)" << endl;
      cout << "Let's Get Started!" << endl;
-     char yn ='n';
+     char yn ;
 
      cout<<"Default Game Setting"<< endl;
      cout<<"-----------------"<< endl;
 
-    avz::Gameboard gb = avz::Gameboard(3, 19);
-	
-	avz::Alien alien = avz::Alien(gb);
-	gb.placeAlien(alien,gb.getColumnCount()/2,gb.getRowCount()/2);
-	avz::Control control(gb,alien);
+    avz::Gameboard gb = avz::Gameboard(5, 9);
     // cout<<"Board Rows:"<< getRowCount<<endl;
     // cout<<"Board Columns:"<< getColumnCount<<endl;
      //cout<<"Zombie Count:"<<getZombieCount<< endl;
@@ -45,14 +38,18 @@ int main()
     
     //srand(1);
 	srand(time(0));
-	
-	int rowCount;
-        int columnCount;
-        int zombieCount;
+	    int x;
+        int y;
+        int z;
 		
 	std::cout << std::endl;
 	if(yn =='N' ||yn =='n')
     {
+        avz::Gameboard gb = avz::Gameboard(5, 9);
+	
+	    avz::Alien alien = avz::Alien(gb);
+	    gb.placeAlien(alien,gb.getColumnCount()/2,gb.getRowCount()/2);
+	    avz::Control control(gb,alien);
         control.work();
     }
 	//gb.show();
@@ -63,30 +60,41 @@ int main()
      cout<<"-----------------"<< endl;
  
         cout<<"Enter row =>";
-        cin>>rowCount;
-        while (rowCount % 2 == 0)
+        cin>>x;
+        while (x % 2 == 0)
         {
             cout<<"Please enter an odd number"<<endl;
             cout<<"Enter row =>";
-            cin>>rowCount;
+            cin>>x;
         }
             cout<<"Enter column =>";
-            cin>>columnCount;
-            while (columnCount % 2 == 0)
+            cin>>y;
+            while (y % 2 == 0)
         {
             cout<<"Please enter an odd number"<<endl;
             cout<<"Enter column =>";
-            cin>>columnCount;
+            cin>>y;
         }
             cout<<endl;
             cout<<"Zombie Setting"<< endl;
             cout<<"-----------------"<< endl;
             cout<<"Enter number of zombie =>";
-            cin>>zombieCount;
+            cin>>z;
             cout<<endl;
             cout<<"Setting Updated."<< endl;
             cout<<"Press any key to continue . . ."<< endl;
+
+            avz::Gameboard gb = avz::Gameboard(x,y);
+	
+	        avz::Alien alien = avz::Alien(gb);
+	        gb.placeAlien(alien,gb.getColumnCount()/2,gb.getRowCount()/2);
+	        avz::Control control(gb,alien);
             control.work();
+             
 
        }
+
+    
+}
+
 
